e6b636c499f5a528455306fdb3909a54e0fcebaa
CesarDQ <cesar@dataqueue.ai>
2025-03-21 02:06:06 +0200

    add new tahg

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Box } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import Checklist from "./components/checklist/Checklist";
 
 function App() {
   return (
-    <Box display="flex" flexDirection="column" height="100%">
-      <Box p={4} fontSize="2rem">
-        Checklist
+    <BrowserRouter basename="/widgettest">
+
+      <Box display="flex" flexDirection="column" height="100%">
+        <Box p={4} fontSize="2rem">
+          Checklist
+        </Box>
+        <Box flex="1 1" p={4}>
+          <Checklist />
+        </Box>
       </Box>
-      <Box flex="1 1" p={4}>
-        <Checklist />
-      </Box>
-    </Box>
+    </BrowserRouter>
   );
 }
 
