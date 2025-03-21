55657e69ac65fe97b897b66e301e1ef1acc7c81d
CesarDQ <cesar@dataqueue.ai>
2025-03-21 02:32:41 +0200

    reset again

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/checklist/i);
+  const linkElement = screen.getByText(/Checklist/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import { Box } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
 import Checklist from "./components/checklist/Checklist";
 
 function App() {
   return (
-    <BrowserRouter basename="/widgettest">
 
-      <Box display="flex" flexDirection="column" height="100%">
-        <Box p={4} fontSize="2rem">
-          Checklist
-        </Box>
-        <Box flex="1 1" p={4}>
-          <Checklist />
-        </Box>
+    <Box display="flex" flexDirection="column" height="100%">
+      <Box p={4} fontSize="2rem">
+        Checklist
       </Box>
-    </BrowserRouter>
+      <Box flex="1 1" p={4}>
+        <Checklist />
+      </Box>
+    </Box>
+
   );
 }
 
